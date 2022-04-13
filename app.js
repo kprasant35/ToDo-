739b35da26f3818aea159beebe95e0999a0a7bba
unknown <prasantkumar.bt19ece@pec.edu.in>
2022-04-13 19:05:16 +0530

    first commit fully working Todo list v1

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+let app=express();
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+
+var items=[];
+
+app.get("/",function(req,res){
+    const date=new Date();
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var day = date.toLocaleDateString("en-US",options);
+
+    res.render("list",{ejsDay : day, ejsItems : items});
+
+});
+
+app.post("/", function(req,res){
+    items.push( req.body.item );
+
+    res.redirect("/");
+});
+
+app.listen(3000,function(){
+    console.log("Server is up..!!");
+});
